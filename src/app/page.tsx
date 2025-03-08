50e2c9f67930103c2fffb0163c267a1e34991632
Murilo Bez Chleba <murilo@MacBook-Pro-de-Murilo.local>
2025-03-08 02:35:02 -0300

    botao search e comeco home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
+import { SearchButton } from "@/components/serachButton";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div>
+      <header className="flex justify-between bg-amber-50 text-black p-6">
+        <h1 className="text-xl">Logo</h1>
+        <SearchButton/>
+      </header>
+      
+      
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start mt-1000">
         <Image
           className="dark:invert"
           src="/next.svg"
